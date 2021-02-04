5216abcc298464ed5a6f0b2c14c10543b4fda774
harleywaagmeester@gmail.com <nsa@nsa.international>
2021-02-04 17:43:52 -0600

    install-ip now creates and populates the website sub-directory struture

--- src/ip.go
+++ src/ip.go
@@ -4,314 +4,316 @@
 package main
 
 import(
-	"fmt"
-	"os"
-	"log"
-	"io"
-	"os/exec"
-	"strings"
-	"encoding/json"
+        "fmt"
+        "os"
+        "log"
+        "io"
+        "os/exec"
+        "strings"
+        "encoding/json"
 )
 
 // The date is updated automatically by a user emacs function named insert-timestamp.
 const (
-	version = "System info:<br>ipgo.go compiled on this date:::Sun Jan 31 20:05:21 2021"
+        version = "System info:<br>ipgo.go compiled on this date:::Thu Feb  4 16:57:49 2021"
 
 )
 
-var (
-    Trace   *log.Logger
-    Info    *log.Logger
-    Warning *log.Logger
-    Error   *log.Logger
-)
+/////////////////////  Functions defining the IPGO language hosted on HTML and CSS.  ////////////////////////////////////////////////////////////
 
-func log_system_init(
-	traceHandle io.Writer,
-	infoHandle io.Writer,
-	warningHandle io.Writer,
-	//	errorHandle io.Writer
-	file string) {
-
-	errorHandle, _ := os.OpenFile(file, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-
-	Trace = log.New(traceHandle,
-        "TRACE: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
-
-    Info = log.New(infoHandle,
-        "INFO: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
-
-    Warning = log.New(warningHandle,
-        "WARNING: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
-
-    Error = log.New(errorHandle,
-        "ERROR: ",
-        log.Ldate|log.Ltime|log.Lshortfile)
-}
-
-type Configuration struct {
-    website_directory string
-}
-
-func read_configuration_file (filename string) string {
-	file, e := os.Open(filename)
-	if e != nil {
-		response_header()
-		cat("../html/error.html")
-		return ("error")
-	}
-	defer file.Close()
-	decoder := json.NewDecoder(file)
-	configuration := Configuration{}
-	err := decoder.Decode(&configuration)
-	if err != nil {
-		fmt.Println("error:", err)
-	}
-	return configuration.website_directory
-
-
-}
-
-func response_header(){
-	fmt.Println("Content-type: text/html")
-	fmt.Println("")
-}
-
-
-
-func system_command(command ...string) int{
-	prog,err:= exec.LookPath(command[0])
-	//	fmt.Println(prog," ",command[1])
-	if err != nil {
-		fmt.Println("ERROR:",err)
-		return 1
-	}
-	//fmt.Println("<pre>")
-	cmd := &exec.Cmd {
-		Path: prog,
-			Args: []string{command[0],command[1]},
-			Stdout: os.Stdout,
-			Stderr: os.Stdout,
-		}
-	if err := cmd.Run(); err != nil {
-		fmt.Println("ERROR:",err)
-	}
-	//fmt.Println("</pre>")
-	return 0
-}
-			
-func cat(fname string) {
-
-	fh, err := os.Open(fname)
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	_, err = io.Copy(os.Stdout, fh)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-
-// Functions defining the IPGO language hosted on HTML and CSS.
-
-
-// use flex for floats
 func flexbox () {
-	fmt.Println("<div class='flexbox';>")
+        fmt.Println("<div class='flexbox';>")
 }
 func flexbox_off () {
-	fmt.Println("</div>")
+        fmt.Println("</div>")
 }
 func floatbox () {
-	fmt.Println("<div class='floatbox';>")
+        fmt.Println("<div class='floatbox';>")
 }
 func floatbox_off () {
-	fmt.Println("</div>")
+        fmt.Println("</div>")
 }
 // span colors
 func color (color string) {
-	if color == "green" {
-		fmt.Println("<span class=span_green>")
-	}
-	if color == "off" {
-		fmt.Println("</span>")
-	}
+        if color == "green" {
+                fmt.Println("<span class=span_green>")
+        }
+        if color == "off" {
+                fmt.Println("</span>")
+        }
 }
 func div () {
-	fmt.Println("<div>")
+        fmt.Println("<div>")
 }
 func div33 () {
-	fmt.Println("<div class='div33'>")
+        fmt.Println("<div class='div33'>")
 }
 func div66 () {
-	fmt.Println("<div class='div66;'>")
+        fmt.Println("<div class='div66;'>")
 }
 func div33_float_left () {
-	fmt.Println("<div class='div33_float_left'>")
+        fmt.Println("<div class='div33_float_left'>")
 }
 func div66_float_left () {
-	fmt.Println("<div class='div66_float_left;'>")
+        fmt.Println("<div class='div66_float_left;'>")
 }
 // Shorthand for span_float_...
 func float_left () {
-	fmt.Println("<span class='float-left'>")
+        fmt.Println("<span class='float-left'>")
 }
 func float_right () {
-	fmt.Println("<span class='float-right'>")
+        fmt.Println("<span class='float-right'>")
 }
 func float_off () {
-	fmt.Println("</span>")
+        fmt.Println("</span>")
 }
 func br () {
-	fmt.Println("<br>")
+        fmt.Println("<br>")
 }
 func div_off () {
-	fmt.Println("</div>")
+        fmt.Println("</div>")
 }
 
 func ul () {
-	fmt.Println("<ul>")
+        fmt.Println("<ul>")
 }
 
 func ulclose () {
-	fmt.Println("</ul>")
+        fmt.Println("</ul>")
 }
 
 func li (s string) {
-	fmt.Println("<li>" + s + "</li>")
+        fmt.Println("<li>" + s + "</li>")
 }
 
-////////////////////////////////////////////////////////////////////////
+////////////////////////  logging system  ////////////////////////////////////////////////////////
 
-func create_menu() {
-	var host string = "https://nsa.international/"
-	div()
-	ul()
-	li("<a href=" + host + "bin/ip.cgi?host> host</a>")
-	li("<a href=" + host + "bin/ip.cgi?whois> whois</a>")
-	li("<a href=" + host + "bin/ip.cgi?env> env</a>")
-	li("<a href=" + host + "bin/ip.cgi?version> version</a>")
-	li("<a href=" + host + "bin/ip.cgi?help> help</a>")
-	ulclose()
-	div_off()
+var (
+        Trace   *log.Logger
+        Info    *log.Logger
+        Warning *log.Logger
+        Error   *log.Logger
+)
+
+func log_system_init(info_file string, error_file string) {
+
+        errorHandle, _ := os.OpenFile(error_file, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+        infoHandle, _ := os.OpenFile(info_file, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+
+
+        Info = log.New(infoHandle,
+                "INFO: ",
+                log.Ldate|log.Ltime|log.Lshortfile)
+
+
+        Error = log.New(errorHandle,
+                "ERROR: ",
+                log.Ldate|log.Ltime|log.Lshortfile)
+
 }
+
+
+///////////////////////  Operating System interoperations  ///////////////////////////////////////////////////
+
+func system_command(command ...string) int{
+        prog,err:= exec.LookPath(command[0])
+        //      fmt.Println(prog," ",command[1])
+        if err != nil {
+                fmt.Println("ERROR:",err)
+                return 1
+        }
+        //fmt.Println("<pre>")
+        cmd := &exec.Cmd {
+                Path: prog,
+                        Args: []string{command[0],command[1]},
+                        Stdout: os.Stdout,
+                        Stderr: os.Stdout,
+                }
+        if err := cmd.Run(); err != nil {
+                fmt.Println("ERROR:",err)
+        }
+        //fmt.Println("</pre>")
+        return 0
+}
+
+/////////////////////  utilities  //////////////////////////////////////////////
+
+func cat(fname string) {
+
+        fh, err := os.Open(fname)
+        if err != nil {
+                log.Fatal(err)
+        }
+        
+        _, err = io.Copy(os.Stdout, fh)
+        if err != nil {
+                log.Fatal(err)
+        }
+}
+
+
+////////////////////  Configuration file operations  ////////////////////////////////////////////////////
+
+type Configuration struct {
+        Website_url string
+        Website_directory string
+}
+
+func read_configuration_file (filename string, setting string) string {
+        file, e := os.Open(filename)
+        if e != nil {
+                response_header()
+                cat("../html/error.html")
+                return ("error")
+        }
+        defer file.Close()
+        decoder := json.NewDecoder(file)
+        configuration := Configuration{}
+        err := decoder.Decode(&configuration)
+        if err != nil {
+                fmt.Println("error:", err)
+        }
+	if setting == "website_directory" {
+		return configuration.Website_directory
+	}
+	if setting == "website_url" {
+		return configuration.Website_url
+	}
+	return("error")
+}
+
+/////////////////////////// html support  ///////////////////////////////////////////////////
+
+func response_header(){
+        fmt.Println("Content-type: text/html")
+        fmt.Println("")
+}
+
+func create_menu(website_directory string) {
+        div()
+        fmt.Println ("A reflection upon your ip address.")
+        ul()
+        li("<a href=" + website_directory + "/bin/ip.cgi?host> host</a>")
+        li("<a href=" + website_directory + "/bin/ip.cgi?whois> whois</a>")
+        li("<a href=" + website_directory + "/bin/ip.cgi?env> env</a>")
+        li("<a href=" + website_directory + "/bin/ip.cgi?version> version</a>")
+        li("<a href=" + website_directory + "/bin/ip.cgi?help> help</a>")
+        ulclose()
+        div_off()
+}
+
+//////////////////////////// MAIN /////////////////////////////////////////////////
+
 
 // Read in HTML files, create HTML elements. execute external programs.
 
 func main() {
+        var website_directory string = read_configuration_file("../conf/ipgo_config.json", "website_directory")
+        var website_url string = read_configuration_file("../conf/ipgo_config.json", "website_url")
+        if  website_directory != "error" {
+                log_system_init("info.log", "error.log")
+                response_header()
+                cat("../html/ip.html")
+                create_menu(website_url)
+                flexbox()
+                div33()
+                float_left()
+                fmt.Print("REMOTE_ADDR:")
+                float_off()
+                float_right()
+                color("green")
+                fmt.Print(os.Getenv("REMOTE_ADDR"))
+                color("off")
+                float_off()
+                br()
+                //      div_off()
+                //      flexbox_off()
+                //      fmt.Println("QUERY_STRING:", os.Getenv("QUERY_STRING"), "<br>")
+                // for _, e := range os.Environ() {
+                //      //        pair := strings.SplitN(e, "=", 2)
+                //      //        p(pair[0],"=",pair[1])
+                //      fmt.Println(e,"<br>")
+                // }
+                if(strings.EqualFold(os.Getenv("QUERY_STRING"),"version")){
+                        fmt.Println(version,"<br>")
+                }
+                if(strings.EqualFold(os.Getenv("QUERY_STRING"),"host")){
+                        //      flexbox()
+                        //div33()
+                        float_left()
+                        fmt.Print("QUERY_STRING:")
+                        float_off()
+                        float_right()
+                        color("green")
+                        fmt.Print(os.Getenv("QUERY_STRING"))
+                        color("off")
+                        float_off()
+                        div_off()
+                        flexbox_off()
 
-	log_system_init(os.Stdout, os.Stdout, os.Stdout, "error.log")
-	if read_configuration_file("../conf/ipgo_config.json") == "error" {
-		Error.Println("can't read the ../conf/ipgo_config.json file")
-		goto early_exit
-	}
-	response_header()
-	cat("../html/ip.html")
-	create_menu()
-	flexbox()
-	div33()
-	float_left()
-	fmt.Print("REMOTE_ADDR:")
-	float_off()
-	float_right()
-	color("green")
-	fmt.Print(os.Getenv("REMOTE_ADDR"))
-	color("off")
-	float_off()
-	br()
-	//	div_off()
-	//	flexbox_off()
-	//	fmt.Println("QUERY_STRING:", os.Getenv("QUERY_STRING"), "<br>")
-	// for _, e := range os.Environ() {
-	// 	//        pair := strings.SplitN(e, "=", 2)
-	// 	//        p(pair[0],"=",pair[1])
-	// 	fmt.Println(e,"<br>")
-	// }
-	if(strings.EqualFold(os.Getenv("QUERY_STRING"),"version")){
-		fmt.Println(version,"<br>")
-	}
-	if(strings.EqualFold(os.Getenv("QUERY_STRING"),"host")){
-		//	flexbox()
-		//div33()
-		float_left()
-		fmt.Print("QUERY_STRING:")
-		float_off()
-		float_right()
-		color("green")
-		fmt.Print(os.Getenv("QUERY_STRING"))
-		color("off")
-		float_off()
-		div_off()
-		flexbox_off()
+                        //              flexbox()
+                        floatbox()
+                        div33_float_left()
+                        fmt.Print("&nbsp")
+                        div_off()
+                        div66_float_left()
+                        //float_right()
+                        system_command("host",os.Getenv("REMOTE_ADDR"))
+                        //              div_off()
+                        floatbox_off()
+                        //float_off()
+                        //              flexbox_off()
+                }
+                if(strings.EqualFold(os.Getenv("QUERY_STRING"),"whois")){
+                        //      flexbox()
+                        //div33()
+                        float_left()
+                        fmt.Print("QUERY_STRING:")
+                        float_off()
+                        float_right()
+                        color("green")
+                        fmt.Print(os.Getenv("QUERY_STRING"))
+                        color("off")
+                        float_off()
+                        div_off()
+                        flexbox_off()
 
-		//		flexbox()
-		floatbox()
-		div33_float_left()
-		fmt.Print("&nbsp")
-		div_off()
-		div66_float_left()
-		//float_right()
-		system_command("host",os.Getenv("REMOTE_ADDR"))
-		//		div_off()
-		floatbox_off()
-		//float_off()
-		//		flexbox_off()
-	}
-	if(strings.EqualFold(os.Getenv("QUERY_STRING"),"whois")){
-		//	flexbox()
-		//div33()
-		float_left()
-		fmt.Print("QUERY_STRING:")
-		float_off()
-		float_right()
-		color("green")
-		fmt.Print(os.Getenv("QUERY_STRING"))
-		color("off")
-		float_off()
-		div_off()
-		flexbox_off()
+                        //              flexbox()
+                        // floatbox()
+                        // div33_float_left()
+                        // fmt.Print("&nbsp")
+                        // div_off()
+                        // div66_float_left()
+                        //float_right()
+                        fmt.Println("<pre>")
+                        system_command("whois",os.Getenv("REMOTE_ADDR"))
+                        fmt.Println("</pre>")
+                        //              div_off()
+                        //              floatbox_off()
+                        //float_off()
+                        //              flexbox_off()
+                }
+                if(strings.EqualFold(os.Getenv("QUERY_STRING"),"env")){
+                        fmt.Println("The environment function is disabled for security reasons.<br>")
+                        // fmt.Println("<pre>")
+                        // fmt.Println("environment\n\n")
+                        // for _, pair := range os.Environ() {
+                        //      fmt.Println(pair)}
+                        // fmt.Println("</pre>")
+                }
 
-		//		flexbox()
-		// floatbox()
-		// div33_float_left()
-		// fmt.Print("&nbsp")
-		// div_off()
-		// div66_float_left()
-		//float_right()
-		fmt.Println("<pre>")
-		system_command("whois",os.Getenv("REMOTE_ADDR"))
-		fmt.Println("</pre>")
-		//		div_off()
-		//		floatbox_off()
-		//float_off()
-		//		flexbox_off()
-	}
-	if(strings.EqualFold(os.Getenv("QUERY_STRING"),"env")){
-		fmt.Println("The environment function is disabled for security reasons.<br>")
-		// fmt.Println("<pre>")
-		// fmt.Println("environment\n\n")
-		// for _, pair := range os.Environ() {
-		// 	fmt.Println(pair)}
-		// fmt.Println("</pre>")
-	}
+                if(strings.EqualFold(os.Getenv("QUERY_STRING"),"help")){
+                        fmt.Println("NSA // MasterTools provides:<br>")
+                        fmt.Println("ip.cgi?help<br>")
+                        fmt.Println("ip.cgi?version<br>")
+                        fmt.Println("ip.cgi?host<br>")
+                        fmt.Println("ip.cgi?whois<br>")
+                        fmt.Println("ip.cgi?env<br>")
+                }
+                
 
-	if(strings.EqualFold(os.Getenv("QUERY_STRING"),"help")){
-		fmt.Println("NSA // MasterTools provides:<br>")
-		fmt.Println("ip.cgi?help<br>")
-		fmt.Println("ip.cgi?version<br>")
-		fmt.Println("ip.cgi?host<br>")
-		fmt.Println("ip.cgi?whois<br>")
-		fmt.Println("ip.cgi?env<br>")
-	}
-	
-
-early_exit:
-	fmt.Println("</div>")
-	fmt.Println("</body>")
-	fmt.Println("</html>")
+                fmt.Println("</div>")
+                fmt.Println("</body>")
+                fmt.Println("</html>")
+        }
 }
